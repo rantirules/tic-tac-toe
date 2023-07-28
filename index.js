8ee04ffb956b9450973fb04308aa44beb6b6a79c
rantirules <oa740@nyu.edu>
2023-07-28 11:59:09 +0100

    mvp of tic tac toe

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ running checkIfWinner function after each player makes a move; if there's a winn
 // }
 
 let board = [
-    ["X", " ", " "],
-    ["O", "X", "O"],
-    ["X", "O", "X"],
+    ["   ", "   ", "   "],
+    ["   ", "   ", "   "],
+    ["   ", "   ", "   "],
 ]
 
 const displayBoard = board => {
+    console.clear()
     for(let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             process.stdout.write(`| ${board[i][j]} |`)
@@ -28,34 +29,32 @@ const displayBoard = board => {
         process.stdout.write("\n")
     }
 }
-// displayBoard(board)
-console.log(`Welcome to tic tac toe!`)
-displayBoard(board) // displaying the empty board
 
-const playerMove = () => {
+
+const playerMove = (playerTurn) => {
     let xCord, yCord
     // we need to make sure player choice is not already taken
     do {
-        let player1input = prompt("Player 1's turn: (e.g 1,1)")
-        if (!validInputChecker(player1input)) {
+        let playerInput = prompt(`Player ${playerTurn}'s turn: (e.g 1,1)`)
+        if (!validInputChecker(playerInput)) {
             console.log("Invalid input")
             continue
 
         }
-        let player1Choice = player1input.split(",")
+        let playerChoice = playerInput.split(",")
 
-        xCord = player1Choice[0]
-        yCord = player1Choice[1]
-        // if (board[xCord][yCord] == " ") {
-        //     board[xCord][yCord] = " X"
-        // }
-    } while(board[xCord][yCord] != " ")
+        xCord = playerChoice[0]
+        yCord = playerChoice[1]
+        
+    } while(board[xCord][yCord] != "   ")
 
-    board[xCord][yCord] = " X "
+    board[xCord][yCord] = ` ${playerTurn} `
 }
 
 const validInputChecker = str => {
-    if (str.length !== 3) {
+    if (!str) {
+        return false
+    } else if (str.length !== 3) {
         return false
     } else if (str.charAt(1) !== "," ) {
         return false
@@ -67,26 +66,26 @@ const validInputChecker = str => {
     return true
 }
 
-displayBoard(board)
+
 
 const checkWin = board => {
     const n = 3; // Size of the Tic Tac Toe board (3x3)
   
     // Check rows and columns
     for (let i = 0; i < n; i++) {
-      if (board[i][0] !== ' ' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+      if (board[i][0] !== '   ' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
         return true; // Row win
       }
-      if (board[0][i] !== ' ' && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+      if (board[0][i] !== '   ' && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
         return true; // Column win
       }
     }
   
     // Check diagonals
-    if (board[0][0] !== ' ' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+    if (board[0][0] !== '   ' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
       return true; // Diagonal from top-left to bottom-right win
     }
-    if (board[0][2] !== ' ' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+    if (board[0][2] !== '   ' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
       return true; // Diagonal from top-right to bottom-left win
     }
   
@@ -94,8 +93,28 @@ const checkWin = board => {
     return false;
   }
 
-  console.log(checkWin(board))
+  
 
+  const main = () => {
+    let turnCount=0, player1Score = 0, player2Score = 0, playerTurn = "O"
+    console.log(`Welcome to tic tac toe!`)
+
+    do {
+        playerTurn = (playerTurn === 'X' ? 'O' : 'X')
+        displayBoard(board)
+        playerMove(playerTurn)
+        turnCount++
+    } while(!checkWin(board) && turnCount < 8)
+    if (turnCount === 8) {
+        console.log(`It's a draw`)
+    } else {
+        playerTurn === "X" ? player1Score++ : player2Score++
+        displayBoard(board)
+        console.log(`Player ${playerTurn} won the game. ${playerTurn}'s score = ${player1Score} and ${playerTurn === 'X' ? 'O' : 'X'}'s score = ${player2Score}`)
+    }
+
+  }
+  main()
 
 
 
